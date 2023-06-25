40ef44c2df1dcb45f8fc67b5b550b6291da35f62
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-06-25 03:45:18 +0200

    pv output numbers calculation v1

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import threading
+from datetime import timedelta
+
 from fastapi import FastAPI
 from solarcalc import *
 from fastapi.middleware.cors import CORSMiddleware
 from misc import *
+from weather_cron import *
 
-API_TOKEN = "8f0830ca7113e6565417609a26f7850e"
 origins = ["http://localhost:3000"]
 
 app = FastAPI()
@@ -16,6 +19,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+thread = threading.Thread(target=run_schedule, args=())
+thread.start()
+
 # Open the ASHRAE Model file
 with open('ASHRAE.json', 'r') as file:
     # Load the JSON data into a dictionary
@@ -25,45 +31,6 @@ with open('ASHRAE.json', 'r') as file:
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/irradiance/date={year}:{month}:{day}:{hour}&cor={lat},{lon}&or={orientation}&angle={angle}")
-async def irradiance(lat: float, lon: float, year: int, month: int, day: int, hour: int, orientation: str,
-                     angle: float):
-    diff = get_utc_offset(30.2, -84.981)  # get_utc_offset(lat, lon)
-    print("UTC Offset", diff)
-    # longitude
-    lon = -84.981
-    # latitude
-    lat = 30.2
-    # input date
-    date = datetime.datetime(year, month, day, hour)
-    print("Date: ", date)
-    # fixed date with UTC Offset
-    fixed_date = datetime.datetime(year, month, day, hour, tzinfo=datetime.timezone.utc) - datetime.timedelta(
-        hours=-1 * diff)
-    print("Date with UTC Offset: ", fixed_date)
-    # local solar time
-    ltsm = get_local_standard_meridian(diff)
-    print("Local Standard Meridian: ", ltsm)
-    eot = equation_of_time(date.timetuple().tm_yday)
-    print("Equation Of Time: ", eot)
-    tc = get_time_correction(lon, ltsm, eot)
-    print("Time Correction: ", tc)
-    local_solar_time = get_local_solar_time(tc, date)
-    print("Local Solar Time: ", local_solar_time)
-    hra = get_hra(local_solar_time)
-    print("hour angle: ", hra)
-    declination = get_dec(date)
-    print("Declination: ", declination)
-    solar_altitude = sun_altitude(declination, lat, hra)
-    print("Elevation: ", solar_altitude)
-    zenith = solar_zenith(solar_altitude)
-    print("Solar Zenith", zenith)
-    azimuth = get_solar_azimuth(zenith, hra, declination, lat, local_solar_time.hour)
-    print("Azimuth: ", azimuth)
-    aoi = angle_oi(angle, surface_azimuth(orientation), solar_altitude, azimuth)
-    print("Aoi: ", aoi)
 
 
 @app.get("/locationsearch&q={query}")
@@ -117,64 +84,106 @@ def get_utc_offset(lat: float, long: float):
     return int(r.json().get('TimeZones')[0].get('ReferenceTime').get('StandardOffset').split(":")[0])
 
 
-@app.post("/syncweather/")
-async def fetch_weather_sync(loc: Loc):
+@app.post("/forceweather")
+async def force_weather(data: QueData):
+    products = []
+    # Get today's date
+    today = datetime.datetime.now().date()
 
-    current_datetime = datetime.datetime.now()
+    # Calculate the date of yesterday
+    yesterday = today - timedelta(days=1)
 
-    target_end = datetime.datetime(loc.end.year, loc.end.month, loc.end.day,loc.end.hour)
+    days_to_add = data.duration  # Replace with the desired number of days
+    # Add the specified number of days
+    target_start = yesterday - timedelta(days=days_to_add)
 
-    current_datetime = current_datetime.replace(minute=0, second=0, microsecond=0)
+    dates_between = get_dates_between(target_start, yesterday)
 
-    if (target_end > current_datetime):
-        target_end = current_datetime
-        #TODO: MAKE USE OF THE DURATION TO KNOW HOW MUCH TO RETRIEVE WHEN SYNCING
+    dates_already = get_dates_from_weather_data(data.idProj)
+    dates_to_retrieve = []
 
+    for i in dates_between:
+        if i not in dates_already:
+            dates_to_retrieve.append(i)
+    products_db = get_products(data.idProj)
 
-    path = f"https://history.openweathermap.org/data/2.5/history/city?lat={loc.lat}&lon={loc.lon}&type=hour&start={date_to_unix_time(loc.start.year, loc.start.month, loc.start.day, loc.start.hour)}&end={date_to_unix_time(target_end.year, target_end.month, target_end.day, target_end.hour)}&appid={API_TOKEN}&units=metric"
-    print(path)
-    r = requests.get(path)
-    # Return a response
-    response = r.json()['list']
+    for item in products_db:
+        products.append({
+            "field_product_id": item[0],
+            "lat": item[1],
+            "lon": item[2],
+            "utc": item[3],
+            "tilt": item[4],
+            "orientation": item[5],
+            "company_product_id": item[6],
+            "weather_data": []
+        })
 
-    last_retrieved = unix_to_normal_time(response[len(response) - 1]['dt'])
+    print(dates_to_retrieve)
+    print(dates_already)
 
-    while not (
-            (last_retrieved[0] == loc.end.year) and
-            (last_retrieved[1] == loc.end.month) and
-            (last_retrieved[2] == loc.end.day) and
-            (last_retrieved[3] == loc.end.hour)
-    ):
-        path = f"https://history.openweathermap.org/data/2.5/history/city?lat={loc.lat}&lon={loc.lon}&type=hour&start={date_to_unix_time(last_retrieved[0], last_retrieved[1], last_retrieved[2], last_retrieved[3])}&end={date_to_unix_time(loc.end.year, loc.end.month, loc.end.day, loc.end.hour)}&appid={API_TOKEN}&units=metric"
-        r = requests.get(path)
-        addon_res = r.json()['list']
+    for product in products:
+        product['weather_data'] = get_already_existent_weather_data(product['field_product_id'], data.idProj)
 
+        for d in dates_to_retrieve:
+            product['weather_data'] += get_weather_data(product['lat'], product['lon'], d)
 
-        last_retrieved = unix_to_normal_time(addon_res[len(addon_res) - 1]['dt'])
+    cprods = get_company_products()
+    # start_cal_force(products, cprods, data.idProj)
 
-        response = response + addon_res
+    update_proj_status(data.idProj)
 
-
-    db_results = []
-    for weather in response:
-        time = weather['dt']
-
-        weather_data = {
-            "time": unix_to_normal_time(time),
-            "lon": loc.lon,
-            "lat": loc.lat,
-            "temp": weather['main']['temp'],
-            "clouds": weather['clouds']['all'],
-            #"utc": loc.utc
-        }
-
-        db_results.append(weather_data)
-
-    print(db_results)
-
-    return response
+    return "Done"
 
 
-#TODO RETRIEVE YESTERDAY WEATHER
+@app.post("/forcesync")
+async def force_sync(data: user):
+    print(data.userID)
+    stat = get_projects_id(data.userID)
+    projects = stat[0]
+    calc_projects = stat[1]
 
-# TODO FIGURE OUT CRON JOB AND HOW TO ACTIVATE THE JOB
+    if len(projects) == 0:
+        return
+
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    cursor = db.cursor()
+    for project in projects:
+        for product in project['products']:
+            weather = get_weather_data_one_day(product['lat'], product['lon'],
+                                               datetime.datetime.now() - datetime.timedelta(days=1))
+
+            for w in weather:
+                time = w['dt']
+
+                weather_data = {
+                    "time": unix_to_normal_time(time),
+                    "lon": product['lon'],
+                    "lat": product['lat'],
+                    "temp": w['main']['temp'],
+                    "clouds": w['clouds']['all'],
+                }
+
+                query = f'INSERT INTO weather_data (date_time,project_id,product_id, temp, clouds ) VALUES (%s,%s,%s,%s,%s)'
+
+                cursor.execute(query, (
+                    yesterday.replace(hour=weather_data['time'][3], minute=0, second=0), project['project_id'],
+                    product['field_product_id'], weather_data['temp'], weather_data['clouds']))
+                db.commit()
+                # Commit the changes to the database
+
+    db.commit()
+    cursor.close()
+
+    if len(calc_projects) == 0:
+        return
+
+    print("Calculation is needed")
+
+    company_products = get_company_products()
+
+    for project in calc_projects:
+        start_cal_naturally(project, company_products)
+        update_proj_status(project['project_id'])
+
+    return "Done"
